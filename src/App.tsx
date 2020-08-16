a98c08ed5f9433a6bd9deb24771bc9ec1f6a7b7f
Maick Souza <maick_a_s@msn.com>
2020-08-15 21:56:05 -0300

    refactor(web): Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,12 @@
 import React, { AnchorHTMLAttributes } from 'react';
-import styled from 'styled-components';
 
 import Burguer from './components/Burguer';
+import Menu from './components/Menu';
 
 type ToggleOpenModalProps = AnchorHTMLAttributes<HTMLAnchorElement> & {
   open?: boolean;
   setOpen?: (open: true | false) => void;
 }
-
-const StyledMenu = styled.nav<ToggleOpenModalProps>`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  background: #EFFFFA;
-  transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
-  height: 100vh;
-  text-align: left;
-  padding: 2rem;
-  position: absolute;
-  top: 0;
-  left: 0;
-  transition: transform 0.3s ease-in-out;
-
-  @media (max-width: 576px) {
-      width: 100%;
-    }
-
-  a {
-    font-size: 2rem;
-    text-transform: uppercase;
-    padding: 2rem 0;
-    font-weight: bold;
-    letter-spacing: 0.5rem;
-    color: #0D0C1D;
-    text-decoration: none;
-    transition: color 0.3s linear;
-
-    @media (max-width: 576px) {
-      font-size: 1.5rem;
-      text-align: center;
-    }
-
-    &:hover {
-      color: #343078;
-    }
-  }
-`
-
-const Menu: React.FC<ToggleOpenModalProps> = ({ open }) => {
-  return (
-    <StyledMenu open={open}>
-      <a href="/">
-        <span role="img" aria-label="about us">💁🏻‍♂️</span>
-        About us
-      </a>
-      <a href="/">
-        <span role="img" aria-label="price">💸</span>
-        Pricing
-        </a>
-      <a href="/">
-        <span role="img" aria-label="contact">📩</span>
-        Contact
-        </a>
-    </StyledMenu>
-  )
-}
-
-
-
-
 
 const App : React.FC<ToggleOpenModalProps> = () => {
   const [isOpen, setIsOpen] = React.useState(false);
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,2 +1,29 @@
-import React from 'react';
+import React, { AnchorHTMLAttributes } from 'react';
 
+import { StyledMenu } from './styles';
+
+export type ToggleOpenModalProps = AnchorHTMLAttributes<HTMLAnchorElement> & {
+  open?: boolean;
+  setOpen?: (open: true | false) => void;
+}
+
+const Menu: React.FC<ToggleOpenModalProps> = ({ open }) => {
+  return (
+    <StyledMenu open={open}>
+      <a href="/">
+        <span role="img" aria-label="about us">💁🏻‍♂️</span>
+        About us
+      </a>
+      <a href="/">
+        <span role="img" aria-label="price">💸</span>
+        Pricing
+        </a>
+      <a href="/">
+        <span role="img" aria-label="contact">📩</span>
+        Contact
+        </a>
+    </StyledMenu>
+  )
+}
+
+export default Menu;
